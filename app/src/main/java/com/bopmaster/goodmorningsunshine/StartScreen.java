0d1d5f27c7fbf012be2cc70031f49e10773c5e0e
Minh Nguyen <tuanminh.nguyen@hotmail.fi>
2017-04-19 14:48:42 +0300

    commit for fresco
    Added Fresco

--- app/src/main/java/com/bopmaster/goodmorningsunshine/StartScreen.java
+++ app/src/main/java/com/bopmaster/goodmorningsunshine/StartScreen.java
@@ -3,11 +3,14 @@ package com.bopmaster.goodmorningsunshine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 public class StartScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        Fresco.initialize(this);
     }
 }
